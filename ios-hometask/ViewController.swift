48956c09d4f34b8ba109db2238c7130547b7a54a
test1 <ponchik0108@icloud.com>
2023-10-08 19:31:22 +0500

    Initial Commit

---
+++ ios-hometask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-hometask
+//
+//  Created by Polina Popova on 08/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
